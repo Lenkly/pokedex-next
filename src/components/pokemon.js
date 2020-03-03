fb1ff5d66422d2046e19b26c6f52dbe03af20a61
lenkly <lenk.lydia@gmail.com>
2020-03-03 16:09:48 +0100

    Add Changes that somehow work but not really

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -1,5 +1,15 @@
-export function pokemon() {
-  const element = document.createElement('div');
-  element.innerText = 'Ampharos';
-  return element;
+import { createElement } from '../lib/dom';
+
+export function pokemons(items) {
+  const container = createElement('div', {
+    className: 'pokemon'
+  });
+  items.forEach(item => {
+    const element = createElement('div', {
+      innerText: item,
+      className: 'pokemon'
+    });
+    container.appendChild(element);
+  });
+  return container;
 }
