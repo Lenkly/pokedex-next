c8bb5cb401ff9ab8b5f62056afb81e0b54fb1245
lenkly <lenk.lydia@gmail.com>
2020-03-03 12:08:39 +0100

    Add a nicer layout

--- src/components/title.js
+++ src/components/title.js
@@ -1,19 +1,9 @@
 import { createElement } from '../lib/dom';
 import './title.scss';
-/*
-export function title() {
-  const titleAttributes = {
-    innerText: 'The next-gen Pokedex',
-    className: 'title'
-  };
-  const element = createElement('h1', titleAttributes);
-
-  return element;
-} */
 
 export function title() {
   const element = createElement('h1', {
-    innerText: 'Pokedex for professionals',
+    innerText: 'Pokedex for Professionals',
     className: 'title'
   });
   return element;
