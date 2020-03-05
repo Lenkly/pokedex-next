a469123df4c5a8a898c53c85e042c352167dffe3
lenkly <lenk.lydia@gmail.com>
2020-03-05 15:08:25 +0100

    Add exchangeable team by click

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -1,21 +1,17 @@
 import './pokemon.scss';
 import { createElement, appendContent } from '../lib/dom';
 
-export function createPokemonList(items) {
-  // ich bekomme ein Kind und nenne es wie ich will hier nenne ich mein Kind (Was ein Array mit Strings innen ist) einfach mal eingabeDerGefiltertenPokemonAlsArray
+export function createPokemonList(pokis) {
   const container = createElement('div', {
     className: 'pokemons'
   });
-  items.forEach(item => {
+  pokis.items.forEach(item => {
     const element = createElement('div', {
       innerText: item,
       className: 'pokemon'
     });
     element.addEventListener('click', () => {
-      const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-      favorites.push(item);
-      const favoriteJSON = JSON.stringify(favorites);
-      localStorage.setItem('favorites', favoriteJSON);
+      pokis.onSearchResultClick(item);
     });
     appendContent(container, element);
   });
